3a22fe45a2b4eabef5eecaf29c93a493b0a16111
erv2bh <pagaous@gmail.com>
2024-03-16 16:47:47 +0900

    feat: add check and logout endpoint

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,9 +1,12 @@
 const express = require("express");
 
 const authController = require("../controllers/auth.controller");
+const { verifyToken } = require("../middlewares/verifyToken");
 
 const router = express.Router();
 
 router.post("/google", authController.google);
+router.post("/check", verifyToken, authController.check);
+router.post("/logout", authController.logout);
 
 module.exports = router;
