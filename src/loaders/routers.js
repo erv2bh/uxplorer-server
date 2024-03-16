e05ae87a2319aed780acb2d0ab9309e0430fb376
erv2bh <pagaous@gmail.com>
2024-03-16 16:48:39 +0900

    feat: add users router

--- src/loaders/routers.js
+++ src/loaders/routers.js
@@ -1,7 +1,9 @@
 const authRouter = require("../routes/auth");
+const userRouter = require("../routes/users");
 
 async function routerLoader(app) {
   app.use("/auth", authRouter);
+  app.use("/users", userRouter);
 }
 
 module.exports = routerLoader;
