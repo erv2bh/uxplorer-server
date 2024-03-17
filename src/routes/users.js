1d70f4eb8110d3dfbb7a5855a503bbb3e01c8cb9
erv2bh <pagaous@gmail.com>
2024-03-17 19:32:14 +0900

    feat: add getTest and getUserMissionDetails router

--- src/routes/users.js
+++ src/routes/users.js
@@ -7,4 +7,11 @@ const router = express.Router();
 router.get("/:userid/tests", testController.getAllTests);
 router.post("/:userid/tests", testController.createTest);
 
+router.get("/:userid/tests/:testid", testController.getTest);
+
+router.get(
+  "/:userid/tests/:testid/missions",
+  testController.getUserMissionDetails,
+);
+
 module.exports = router;
