0670b2fcf2adcd972065b304f4b1bd56b7946367
erv2bh <pagaous@gmail.com>
2024-03-18 15:46:02 +0900

    feat: add isLoggedIn field in tester schema

--- src/models/Tester.js
+++ src/models/Tester.js
@@ -14,6 +14,11 @@ const testerSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  isLoggedIn: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
   missions: [
     {
       type: mongoose.Schema.Types.ObjectId,
